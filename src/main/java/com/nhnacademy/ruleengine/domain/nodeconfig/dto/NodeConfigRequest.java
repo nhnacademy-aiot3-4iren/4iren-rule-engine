7f2c93d6d99a0d644cb35a351c716f2d5efb404e
hjiwon203 <hjiwon203@gmail.com>
2026-07-29 17:44:00 +0900

    feat(nodeConfig) 조회/수정 서비스 및 dto 구현

--- src/main/java/com/nhnacademy/ruleengine/domain/nodeconfig/dto/NodeConfigRequest.java
+++ src/main/java/com/nhnacademy/ruleengine/domain/nodeconfig/dto/NodeConfigRequest.java
@@ -3,7 +3,6 @@ package com.nhnacademy.ruleengine.domain.nodeconfig.dto;
 import com.nhnacademy.ruleengine.domain.flow.enums.NodeType;
 
 public record NodeConfigRequest(
-        Long tempNodeId,
         NodeType nodeType
 ) {
 }
