5b0ee5c2736d57a2890b349d80e99f9c6bbe25eb
hjiwon203 <hjiwon203@gmail.com>
2026-07-29 12:20:14 +0900

    feat(nodeConfig) node_config 인터페이스 및 하위 dto 구현

---
+++ src/main/java/com/nhnacademy/ruleengine/domain/nodeconfig/impl/ThresholdNodeConfig.java
@@ -0,0 +1,33 @@
+package com.nhnacademy.ruleengine.domain.nodeconfig.impl;
+
+
+import com.nhnacademy.ruleengine.common.enums.Operator;
+import com.nhnacademy.ruleengine.domain.flow.enums.NodeType;
+import com.nhnacademy.ruleengine.domain.flow.enums.SensorType;
+import com.nhnacademy.ruleengine.domain.nodeconfig.NodeConfig;
+import jakarta.validation.constraints.NotNull;
+
+public record ThresholdNodeConfig (
+
+        @NotNull
+        NodeType nodeType,
+
+        @NotNull
+        Integer x,
+
+        @NotNull
+        Integer y,
+
+        @NotNull
+        SensorType sensorType,
+        @NotNull
+        Operator operator,
+        @NotNull
+        double threshold,
+
+        String unit,
+
+        String targetSensor
+)implements NodeConfig {
+
+}
