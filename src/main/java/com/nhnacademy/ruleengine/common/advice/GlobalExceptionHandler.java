9497feb1b289d8ee1859f6acdc96fa0980edf8b3
hjiwon203 <hjiwon203@gmail.com>
2026-08-06 17:02:06 +0900

    feat(exception): 플로우 무결성 검증 에러 응답 ExceptionHandler로 처리

--- src/main/java/com/nhnacademy/ruleengine/common/advice/GlobalExceptionHandler.java
+++ src/main/java/com/nhnacademy/ruleengine/common/advice/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.nhnacademy.ruleengine.common.advice;
 
 import com.nhnacademy.ruleengine.common.exception.BusinessException;
 import com.nhnacademy.ruleengine.common.exception.ErrorCode;
+import com.nhnacademy.ruleengine.common.exception.invalid.FlowValidationFailed;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,5 +21,17 @@ public class GlobalExceptionHandler {
                         errorCode.getCode(),
                         errorCode.getMessage()
                 ));
+    }
+
+    @ExceptionHandler(FlowValidationFailed.class)
+    public ResponseEntity<ValidationError> handleValidationError(FlowValidationFailed e){
+
+
+        return ResponseEntity.badRequest()
+                .body(
+                    new ValidationError(
+                            e.getMessage(),
+                            e.getErrors())
+                );
     }
 }
