f4810eecdded360bed4408b570fae19fa5e7becb
hjiwon203 <hjiwon203@gmail.com>
2026-09-07 18:20:00 +0900

    flow(RabbitConfig, FlowLoader): 메시지 수신시 convertor에 도달전까지 역직렬화 시도 방지 및 플로우 없는 경우도 캐시하여 db접근 줄이도록 수정

--- src/main/java/com/nhnacademy/ruleengine/common/config/RabbitConfig.java
+++ src/main/java/com/nhnacademy/ruleengine/common/config/RabbitConfig.java
@@ -8,7 +8,6 @@ import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -38,8 +37,7 @@ public class RabbitConfig {
         return mapper;
     }
 
-    @Bean
-    public MessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
+    private Jackson2JsonMessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
         return new Jackson2JsonMessageConverter(objectMapper);
     }
 
